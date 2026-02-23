d041edc1d772439950accab04e2c03100f046ce8
sujith kumar <eagle.suji@gmail.com>
2026-02-23 23:44:31 +0530

    Fix user-data.sh for Amazon Linux 2023

--- user-data.sh
+++ user-data.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-apt update -y
-apt upgrade -y
+dnf update -y
 
-apt install docker.io -y
+dnf install docker -y
 systemctl start docker
 systemctl enable docker
 
 curl -L "https://github.com/docker/compose/releases/download/v2.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-usermod -aG docker ubuntu
+usermod -aG docker ec2-user
 
-cd /home/ubuntu
+cd /home/ec2-user
 git clone https://github.com/sujith-eag/mean-crud-app.git
 cd mean-crud-app
 
-apt install -y curl wget
+dnf install -y curl wget
+
 
 reboot
