dbaf4e2d055cd5c0342f40038e9646f761d940a2
sujith kumar <eagle.suji@gmail.com>
2026-02-24 19:08:02 +0530

    Changes to user data file for better permission

--- user-data.sh
+++ user-data.sh
@@ -3,10 +3,13 @@
 dnf update -y
 
 dnf install docker -y
+dnf install git -y
+
+dnf install -y curl wget
+
 systemctl start docker
 systemctl enable docker
 
-dnf install git
 
 curl -L "https://github.com/docker/compose/releases/download/v2.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
@@ -14,10 +17,9 @@ chmod +x /usr/local/bin/docker-compose
 usermod -aG docker ec2-user
 
 cd /home/ec2-user
+
 git clone https://github.com/sujith-eag/mean-crud-app.git
+
+chown -R ec2-user:ec2-user /home/ec2-user/mean-crud-app
+
 cd mean-crud-app
-
-dnf install -y curl wget
-
-
-reboot
